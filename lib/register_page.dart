88ed63f968425e35a9cf2009738992716afa51ba
Rafi Alifa Bagja <rafialifa100@gmail.com>
2025-04-27 15:51:35 +0700

    rapihin

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
@@ -10,7 +9,8 @@ class RegisterPage extends StatefulWidget {
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderStateMixin {
+class _RegisterPageState extends State<RegisterPage>
+    with SingleTickerProviderStateMixin {
   final _formKey = GlobalKey<FormState>();
 
   final _fullNameController = TextEditingController();
@@ -23,24 +23,24 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
   bool _showPassword = false;
   bool _showConfirmPassword = false;
   String? _errorMessage;
-  
+
   late AnimationController _animationController;
   late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    
+
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 800),
       vsync: this,
     );
-    
+
     _animation = CurvedAnimation(
       parent: _animationController,
       curve: Curves.easeInOut,
     );
-    
+
     _animationController.forward();
   }
 
@@ -67,10 +67,8 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
 
       try {
         // Register user using Firebase Authentication
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
 
         // Get the newly created user
         final User? user = userCredential.user;
@@ -105,9 +103,7 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
       backgroundColor: Colors.blue[700],
       behavior: SnackBarBehavior.floating,
       elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       margin: const EdgeInsets.all(16),
       duration: const Duration(seconds: 2),
     );
@@ -149,66 +145,59 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
             ),
             child: TextFormField(
               controller: controller,
-              obscureText: isPassword
-                  ? (isConfirm ? !_showConfirmPassword : !_showPassword)
-                  : false,
+              obscureText:
+                  isPassword
+                      ? (isConfirm ? !_showConfirmPassword : !_showPassword)
+                      : false,
               style: const TextStyle(fontSize: 16),
               decoration: InputDecoration(
                 prefixIcon: Icon(icon, color: Colors.blue.shade600),
-                suffixIcon: isPassword
-                    ? IconButton(
-                        icon: Icon(
-                          (isConfirm ? _showConfirmPassword : _showPassword)
-                              ? LucideIcons.eyeOff
-                              : LucideIcons.eye,
-                          color: Colors.grey.shade600,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            if (isConfirm) {
-                              _showConfirmPassword = !_showConfirmPassword;
-                            } else {
-                              _showPassword = !_showPassword;
-                            }
-                          });
-                        },
-                      )
-                    : null,
+                suffixIcon:
+                    isPassword
+                        ? IconButton(
+                          icon: Icon(
+                            (isConfirm ? _showConfirmPassword : _showPassword)
+                                ? LucideIcons.eyeOff
+                                : LucideIcons.eye,
+                            color: Colors.grey.shade600,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (isConfirm) {
+                                _showConfirmPassword = !_showConfirmPassword;
+                              } else {
+                                _showPassword = !_showPassword;
+                              }
+                            });
+                          },
+                        )
+                        : null,
                 filled: true,
                 fillColor: Colors.white,
                 hintText: 'Masukkan $label',
-                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 14,
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.blue.shade200,
-                  ),
+                  borderSide: BorderSide(color: Colors.blue.shade200),
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade200,
-                  ),
+                  borderSide: BorderSide(color: Colors.grey.shade200),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.blue.shade400,
-                    width: 2,
-                  ),
+                  borderSide: BorderSide(color: Colors.blue.shade400, width: 2),
                 ),
                 errorBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.red.shade300,
-                  ),
+                  borderSide: BorderSide(color: Colors.red.shade300),
                 ),
                 focusedErrorBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.red.shade400,
-                    width: 2,
-                  ),
+                  borderSide: BorderSide(color: Colors.red.shade400, width: 2),
                 ),
               ),
               validator: (value) {
@@ -270,15 +259,19 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
                             ),
                           ],
                         ),
-                        child: const Icon(LucideIcons.heart, color: Colors.white, size: 36),
+                        child: const Icon(
+                          LucideIcons.heart,
+                          color: Colors.white,
+                          size: 36,
+                        ),
                       ),
                       const SizedBox(height: 16),
                       Text(
                         "Daftar Akun Baru",
                         style: TextStyle(
-                          fontSize: 24, 
+                          fontSize: 24,
                           fontWeight: FontWeight.bold,
-                          color: Colors.blue.shade700
+                          color: Colors.blue.shade700,
                         ),
                       ),
                       const SizedBox(height: 8),
@@ -423,7 +416,11 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
                           ),
                           child: Row(
                             children: [
-                              Icon(LucideIcons.alertTriangle, color: Colors.red.shade700, size: 20),
+                              Icon(
+                                LucideIcons.alertTriangle,
+                                color: Colors.red.shade700,
+                                size: 20,
+                              ),
                               const SizedBox(width: 10),
                               Expanded(
                                 child: Text(
@@ -448,50 +445,68 @@ class _RegisterPageState extends State<RegisterPage> with SingleTickerProviderSt
                             ),
                             elevation: 2,
                           ),
-                          child: _isLoading
-                              ? const SizedBox(
-                                  width: 24,
-                                  height: 24,
-                                  child: CircularProgressIndicator(
-                                    color: Colors.white,
-                                    strokeWidth: 2,
-                                  ),
-                                )
-                              : const Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(LucideIcons.userPlus),
-                                    SizedBox(width: 10),
-                                    Text("Daftar Sekarang",
+                          child:
+                              _isLoading
+                                  ? const SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white,
+                                      strokeWidth: 2,
+                                    ),
+                                  )
+                                  : const Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(LucideIcons.userPlus),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        "Daftar Sekarang",
                                         style: TextStyle(
                                           fontSize: 16,
                                           fontWeight: FontWeight.w600,
-                                        )),
-                                  ],
-                                ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                         ),
                       ),
                       const SizedBox(height: 20),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Sudah punya akun?",
-                              style: TextStyle(color: Colors.grey.shade700)),
+                          Text(
+                            "Sudah punya akun?",
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
                           TextButton(
-                            onPressed: () => Navigator.pushNamed(context, '/login'),
-                            child: Text("Masuk",
-                                style: TextStyle(
-                                  color: Colors.blue.shade700,
-                                  fontWeight: FontWeight.w600,
-                                )),
+                            onPressed:
+                                () => Navigator.pushNamed(context, '/login'),
+                            child: Text(
+                              "Masuk",
+                              style: TextStyle(
+                                color: Colors.blue.shade700,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
                           ),
                         ],
                       ),
                       TextButton.icon(
-                        onPressed: () => Navigator.pushReplacementNamed(context, '/home'),
-                        icon: Icon(Icons.arrow_back, size: 16, color: Colors.grey.shade600),
-                        label: Text("Kembali ke Beranda",
-                            style: TextStyle(color: Colors.grey.shade600)),
+                        onPressed:
+                            () => Navigator.pushReplacementNamed(
+                              context,
+                              '/home',
+                            ),
+                        icon: Icon(
+                          Icons.arrow_back,
+                          size: 16,
+                          color: Colors.grey.shade600,
+                        ),
+                        label: Text(
+                          "Kembali ke Beranda",
+                          style: TextStyle(color: Colors.grey.shade600),
+                        ),
                       ),
                     ],
                   ),
